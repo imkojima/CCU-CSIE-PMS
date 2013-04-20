1dd38adef48fa066cf16704dda9953404bfae5e9
Chaowen Chang <kevin68x@gmail.com>
2013-04-20 19:53:58 +0800

    -a

--- crontab.php
+++ crontab.php
@@ -5,6 +5,12 @@ require_once('libs/reserve.php');
 /* Crontabl Daily Script */
 $reserves = getReserveList();
 
+for($i=0;$i<count($reserves)-1;$i++){
+	if($reserves[$i]['r_state']==1){
+		echo "->"$reserves[$i]['u_id']." - ".$reserves[$i]['r_days']."<br />";
+	}
+}
+
 echo "<pre>";
 print_r($reserves);
 echo "</pre>";
