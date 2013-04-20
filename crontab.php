63b06e5450a287837078891f81839f1732e3f8f1
Chaowen Chang <kevin68x@gmail.com>
2013-04-20 20:22:51 +0800

    -a

--- crontab.php
+++ crontab.php
@@ -8,7 +8,7 @@ $reserves = getReserveList();
 for($i=0;$i<count($reserves)-1;$i++){
 	//正在等待審核的預約
 	if($reserves[$i]['r_state']==0){
-		echo "-> 預約編號：".$reserves[$i]['u_id']." - 剩餘預約天數：".$reserves[$i]['r_days']." ... ";
+		echo "-> 預約編號：".$reserves[$i]['r_id']." - 剩餘預約天數：".$reserves[$i]['r_days']." ... ";
 		if($reserves[$i]['r_days'] != 0){
 			//遞減它的預約天數
 			setReserveDays( $reserves[$i]['r_id'] , $reserves[$i]['r_days']-1);
