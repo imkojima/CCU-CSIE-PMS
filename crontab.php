5e12f37e78283428ff8c6b4a06523ad1f2a4ebb8
Chaowen Chang <kevin68x@gmail.com>
2013-04-22 21:35:12 +0800

    crontab log implement

--- crontab.php
+++ crontab.php
@@ -1,10 +1,13 @@
 #!/usr/bin/php -q
 <?php
 require_once('libs/reserve.php');
+require_once('libs/logs.php');
 
 /* Crontabl Daily Script */
 $reserves = getReserveList();
 
+$numDecrease = 0;	//遞減預約項目數
+$numCancel = 0;		//取消預約項目數
 for($i=0;$i<count($reserves)-1;$i++){
 	//正在等待審核的預約
 	if($reserves[$i]['r_state']==0){
@@ -13,12 +16,14 @@ for($i=0;$i<count($reserves)-1;$i++){
 			//遞減它的預約天數
 			setReserveDays( $reserves[$i]['r_id'] , $reserves[$i]['r_days']-1);
 			echo "遞減<br/>";
+			$numDecrease++;
 		}else{
 			//因超過預約期限，自動取消預約
 			cancelReserve( $reserves[$i]['r_id'] );	
 			echo "取消<br/>";
+			$numCancel++;
 		}
 	}
 }
-
+makeLog("Crontab", "完成今日排程操作");
 ?>
